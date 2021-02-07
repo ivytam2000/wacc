d4a2517f4865f797d93d16d0e66333c45564de60
Vincent <vincentkcho627@gmail.com>
2021-02-07 19:51:22 +0800

    [kch19] added arrayLiterAST class for arrayLiter rule

--- src/main/frontend/abstractsyntaxtree/ArrayLiterAST.java
+++ src/main/frontend/abstractsyntaxtree/ArrayLiterAST.java
@@ -1,5 +1,30 @@
 package frontend.abstractsyntaxtree;
 
-public class ArrayListAST {
+import frontend.symboltable.Identifier;
+import frontend.symboltable.SymbolTable;
+import frontend.symboltable.TypeID;
+import java.util.List;
 
+public class ArrayLiterAST extends Node {
+
+  private SymbolTable symtab;
+  private List<Node> children;
+
+  public ArrayLiterAST (SymbolTable symtab, List<Node> children) {
+    this.symtab = symtab;
+    this.children = children;
+  }
+
+  @Override
+  public void check() {
+    String type = children.get(0).getIdentifier().getType().getTypeName();
+    for (int i = 1; i < children.size(); i++) {
+      Node child = children.get(i);
+      String childType = child.getIdentifier().getType().getTypeName();
+      if (!childType.equals(type)) {
+        System.out.println("Array doesn't have consistent types, index " + i +
+            " has type " + childType + " but expected " + type);
+      }
+    }
+  }
 }
