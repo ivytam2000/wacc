1cab8d630401ed86655f2279a172e819b5f303a7
Vincent <vincentkcho627@gmail.com>
2021-02-07 19:42:50 +0800

    [kch19] added argListAST class for argList rule

--- src/main/frontend/abstractsyntaxtree/ArgListAST.java
+++ src/main/frontend/abstractsyntaxtree/ArgListAST.java
@@ -1,5 +1,21 @@
 package frontend.abstractsyntaxtree;
 
-public class ArgListAST {
+import frontend.symboltable.Identifier;
+import frontend.symboltable.SymbolTable;
+import java.util.List;
 
+public class ArgListAST extends Node {
+
+  private List<Node> expressions;
+  private SymbolTable symtab;
+
+  public ArgListAST(SymbolTable symtab, List<Node> expressions) {
+    this.symtab = symtab;
+    this.expressions = expressions;
+  }
+
+  @Override
+  public void check() {
+
+  }
 }
