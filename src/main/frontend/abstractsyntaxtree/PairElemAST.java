4823d064be74693bee214a0468b2d4fa57dc620a
Vincent <vincentkcho627@gmail.com>
2021-02-07 19:42:22 +0800

    [kch19] added pairElemAST class for pair elem rule

--- src/main/frontend/abstractsyntaxtree/PairElemAST.java
+++ src/main/frontend/abstractsyntaxtree/PairElemAST.java
@@ -1,5 +1,25 @@
 package frontend.abstractsyntaxtree;
 
-public class PairElemAST {
+import frontend.symboltable.Identifier;
+import frontend.symboltable.SymbolTable;
+
+public class PairElemAST extends Node {
+
+  private SymbolTable symtab;
+  private String pairName;
+  private boolean first;
+  private Node child;
+
+  public PairElemAST(Identifier identifier, SymbolTable symtab, boolean first, Node child) {
+    super(identifier);
+    this.symtab = symtab;
+    this.first = first;
+    this.child = child;
+  }
+
+  @Override
+  public void check() {
+    child.check();
+  }
 
 }
