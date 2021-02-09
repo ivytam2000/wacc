bf6c2d528de6297484133038141c666d00cd2f45
Vincent <vincentkcho627@gmail.com>
2021-02-10 01:59:16 +0800

    [full team] implemented ArrayType AST node

---
+++ src/main/frontend/abstractsyntaxtree/ArrayTypeAST.java
@@ -0,0 +1,26 @@
+package frontend.abstractsyntaxtree;
+
+import frontend.symboltable.Identifier;
+import frontend.symboltable.SymbolTable;
+
+public class ArrayTypeAST extends Node {
+
+  private Identifier identifier;
+  private SymbolTable symtab;
+  private int dimensions;
+
+  public ArrayTypeAST(Identifier identifier, SymbolTable symtab, int dimensions) {
+    super(identifier);
+    this.symtab = symtab;
+    this.dimensions = dimensions;
+  }
+
+  public int getDimensions() {
+    return dimensions;
+  }
+
+  @Override
+  public void check() {
+
+  }
+}
