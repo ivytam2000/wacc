032cff6f17d2ab063fe7274574a2cab48b69836a
Ivy Tam <iat19@ic.ac.uk>
2021-02-12 04:04:41 +0800

    [iat19] Refractored VarDecAST so that it only uses typeCompat

--- src/main/frontend/abstractsyntaxtree/statements/VarDecAST.java
+++ src/main/frontend/abstractsyntaxtree/statements/VarDecAST.java
@@ -17,8 +17,12 @@ public class VarDecAST extends Node {
   private final AssignRHSAST assignRHS;
   private final Var_decl_statContext ctx;
 
-  public VarDecAST(SymbolTable symtab, Node typeAST,
-      String varName, AssignRHSAST assignRHS, Var_decl_statContext ctx) {
+  public VarDecAST(
+      SymbolTable symtab,
+      Node typeAST,
+      String varName,
+      AssignRHSAST assignRHS,
+      Var_decl_statContext ctx) {
     super();
     this.symtab = symtab;
     this.typeAST = typeAST;
@@ -29,80 +33,93 @@ public class VarDecAST extends Node {
 
   @Override
   public void check() {
-    if (typeAST instanceof PairTypeAST) {
-      checkAsPairType();
-    } else if (typeAST instanceof ArrayTypeAST) {
-      checkAsArrayType();
+    TypeID decType = typeAST.getIdentifier().getType();
+    TypeID rhsType = assignRHS.getIdentifier().getType();
+
+    String typeName = decType.getTypeName();
+
+    int line = ctx.getStart().getLine();
+    int pos = ctx.getStart().getCharPositionInLine();
+
+    if (typeAST instanceof PairTypeAST || typeAST instanceof ArrayTypeAST) {
+      if (!Utils.typeCompat(decType, rhsType)) {
+        SemanticErrorCollector.addIncompatibleType(
+            decType.getTypeName(), rhsType.getTypeName(), varName, line, pos);
+      }
     } else {
-      String typeName = typeAST.getIdentifier().getType().getTypeName();
+
       Identifier typeID = symtab.lookupAll(typeName);
       Identifier variable = symtab.lookup(varName);
 
       if (!(typeID instanceof TypeID)) {
         // Check if the identifier returned has a known type
-        SemanticErrorCollector.addUnknownType(typeName, ctx.getStart().getLine(),
-            ctx.getStart().getCharPositionInLine());
+        SemanticErrorCollector.addUnknownType(typeName, line, pos);
       } else if (variable != null && !(variable instanceof FuncID)) {
-        SemanticErrorCollector.addSymbolAlreadyDefined(varName, ctx.getStart().getLine(),
-            ctx.getStart().getCharPositionInLine());
-      } else if (!Utils.typeCompat(typeAST.getIdentifier().getType(), assignRHS.getIdentifier().getType())) {
-        SemanticErrorCollector.addIncompatibleType(typeName, assignRHS.getIdentifier().getType().getTypeName(), varName, ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine());
+        SemanticErrorCollector.addSymbolAlreadyDefined(varName, line, pos);
+      } else if (!Utils.typeCompat(decType, rhsType)) {
+        SemanticErrorCollector.addIncompatibleType(
+            decType.getTypeName(), rhsType.getTypeName(), varName, line, pos);
       }
     }
-
     symtab.add(varName, typeAST.getIdentifier().getType());
     setIdentifier(typeAST.getIdentifier().getType());
   }
 
-  private void checkAsPairType() {
-    TypeID fstType = ((PairTypeAST) typeAST).getFst().getIdentifier().getType();
-    TypeID sndType = ((PairTypeAST) typeAST).getSnd().getIdentifier().getType();
-    TypeID assignType = assignRHS.getIdentifier().getType();
-
-    if (assignType instanceof PairID) {
-      PairID assignRHSAsPair = (PairID) assignType;
-
-      verifyPairElemTypeOfRHS(fstType, assignRHSAsPair.getFstType());
-      verifyPairElemTypeOfRHS(sndType, assignRHSAsPair.getSndType());
-    } else if (!(assignType instanceof NullID)) {
-      SemanticErrorCollector.addIncompatibleType("pair", assignType.getTypeName(),
-          varName, ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine());
-    }
-  }
-
-  private void checkAsArrayType() {
-    TypeID assignType = assignRHS.getIdentifier().getType();
-    if (assignType instanceof ArrayID) {
-      if (!Utils.compareArrayTypes(typeAST.getIdentifier().getType(), assignType)) {
-        String errorMsg = String.format("line %d:%d -- Array doesn't have consistent types, "
-                + "expected type: %s but got actual type: %s",
-            ctx.getStart().getLine(),
-            ctx.getStart().getCharPositionInLine(),
-            typeAST.getIdentifier().getType().getTypeName(), assignType.getTypeName());
-        SemanticErrorCollector.addError(errorMsg);
-      }
-    }
-  }
-
-  private void verifyPairElemTypeOfRHS(TypeID elemType, TypeID elemRHS) {
-    if (elemType instanceof OptionalPairID) {
-      if (!(elemRHS instanceof OptionalPairID)) {
-        SemanticErrorCollector.addError("First of pair : Expected pair");
-      }
-    } else if (elemType instanceof ArrayID) {
-      if (!Utils.compareArrayTypes(elemType, elemRHS)) {
-        SemanticErrorCollector.addError("First of pair : Expected array");
-      }
-    } else {
-      //Null rhs can match with any pair
-      if (!(elemRHS instanceof NullID)) {
-        if (elemType != elemRHS) {
-          SemanticErrorCollector.addError("First of pair : Types mismatch");
-        }
-      }
-    }
-  }
-
+  //  private void checkAsPairType() {
+  //    TypeID fstType = ((PairTypeAST) typeAST).getFst().getIdentifier().getType();
+  //    TypeID sndType = ((PairTypeAST) typeAST).getSnd().getIdentifier().getType();
+  //    TypeID assignType = assignRHS.getIdentifier().getType();
+  //
+  //    if (assignType instanceof PairID) {
+  //      PairID assignRHSAsPair = (PairID) assignType;
+  //
+  //      verifyPairElemTypeOfRHS(fstType, assignRHSAsPair.getFstType());
+  //      verifyPairElemTypeOfRHS(sndType, assignRHSAsPair.getSndType());
+  //    } else if (!(assignType instanceof NullID)) {
+  //      SemanticErrorCollector.addIncompatibleType(
+  //          "pair",
+  //          assignType.getTypeName(),
+  //          varName,
+  //          ctx.getStart().getLine(),
+  //          ctx.getStart().getCharPositionInLine());
+  //    }
+  //  }
+  //
+  //  private void checkAsArrayType() {
+  //    TypeID assignType = assignRHS.getIdentifier().getType();
+  //    if (assignType instanceof ArrayID) {
+  //      if (!Utils.compareArrayTypes(typeAST.getIdentifier().getType(), assignType)) {
+  //        String errorMsg =
+  //            String.format(
+  //                "line %d:%d -- Array doesn't have consistent types, "
+  //                    + "expected type: %s but got actual type: %s",
+  //                ctx.getStart().getLine(),
+  //                ctx.getStart().getCharPositionInLine(),
+  //                typeAST.getIdentifier().getType().getTypeName(),
+  //                assignType.getTypeName());
+  //        SemanticErrorCollector.addError(errorMsg);
+  //      }
+  //    }
+  //  }
+  //
+  //  private void verifyPairElemTypeOfRHS(TypeID elemType, TypeID elemRHS) {
+  //    if (elemType instanceof OptionalPairID) {
+  //      if (!(elemRHS instanceof OptionalPairID)) {
+  //        SemanticErrorCollector.addError("First of pair : Expected pair");
+  //      }
+  //    } else if (elemType instanceof ArrayID) {
+  //      if (!Utils.compareArrayTypes(elemType, elemRHS)) {
+  //        SemanticErrorCollector.addError("First of pair : Expected array");
+  //      }
+  //    } else {
+  //      // Null rhs can match with any pair
+  //      if (!(elemRHS instanceof NullID)) {
+  //        if (elemType != elemRHS) {
+  //          SemanticErrorCollector.addError("First of pair : Types mismatch");
+  //        }
+  //      }
+  //    }
+  //  }
   // TODO: FOR DEBUGGING.
   public Node getTypeAST() {
     return typeAST;
