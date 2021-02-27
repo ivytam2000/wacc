92ae87347619d669cacee34f58a05b213791342c
Ivy Tam <iat19@ic.ac.uk>
2021-02-28 02:14:50 +0800

    [iat19] Modified ADD instr so that its of the form ADD{S} dest, src, immediate instead of ADD{S} dest, dest, operand2

--- src/main/backend/instructions/ADD.java
+++ src/main/backend/instructions/ADD.java
@@ -4,18 +4,25 @@ public class ADD implements Instr {
 
   private final boolean setFlags;
   private final String dest; //Also first operand
-  private final String operand2;
+  private final String src;
+  private final String immediate;
 
-  public ADD(boolean setFlags, String dest, String operand2) {
+  public ADD(boolean setFlags, String dest, String src, String immediate) {
     this.setFlags = setFlags;
     this.dest = dest;
-    this.operand2 = operand2;
+    this.src = src;
+    this.immediate = immediate;
+
   }
 
+
   // ADD{S} dest, dest, operand2
+  //ADD
   @Override
   public String translateToArm() {
     return "ADD" + (setFlags ? "S " : " ")
-        + dest + ", " + dest + ", " + operand2;
+        + dest + ", " + src + ", " + immediate;
+   /* return "ADD" + (setFlags ? "S " : " ")
+        + dest + ", " + dest + ", " + operand2;*/
   }
 }
