cfb1fcd31e3e47206b2d0f9af01acce9e08d492d
Ivy Tam <iat19@ic.ac.uk>
2021-03-03 22:52:31 +0800

    [iat19] Refractored AssignRHS to add instructions in order and account for array elem in pair case

--- src/main/frontend/abstractsyntaxtree/assignments/AssignRHSAST.java
+++ src/main/frontend/abstractsyntaxtree/assignments/AssignRHSAST.java
@@ -9,6 +9,8 @@ import frontend.abstractsyntaxtree.Node;
 import frontend.symboltable.Identifier;
 import frontend.symboltable.PairID;
 import frontend.symboltable.SymbolTable;
+import frontend.symboltable.TypeID;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,32 +39,35 @@ public class AssignRHSAST extends Node {
 
   @Override
   public void toAssembly() {
-    List<Instr> instructions = new ArrayList<>();
+   // List<Instr> instructions = new ArrayList<>();
     if (identifier instanceof PairID) {
       // malloc pair struct
-      instructions.add(new LDR(Instr.R0, "8"));
-      instructions.add(new BRANCH(true, "", "malloc"));
-      instructions.add(new MOV("", Instr.R4, Instr.R0));
+      addToCurLabel(new LDR(Instr.R0, "8"));
+      addToCurLabel(new BRANCH(true, "", "malloc"));
+      addToCurLabel(new MOV("", Instr.R4, Instr.R0));
+      // Need to increment register as we using R4
+      Instr.incDepth();
 
       // first pair elem
       children.get(0).toAssembly();
-      instructions.add(new LDR(Instr.R0, "4"));
-      instructions.add(new BRANCH(true, "", "malloc"));
-      instructions.add(new STR(Instr.R5, Instr.R0, 0));
-      instructions.add(new STR(Instr.R0, Instr.R4, 0));
+      TypeID child_1 = children.get(0).getIdentifier().getType();
+      addToCurLabel(new LDR(Instr.R0, String.valueOf(child_1.getBytes())));
+      addToCurLabel(new BRANCH(true, "", "malloc"));
+      addToCurLabel(new STR(child_1.getBytes(), "", Instr.R5, Instr.R0, 0));
+      addToCurLabel(new STR(Instr.R0, Instr.R4, 0));
 
       // second pair elem
       // if children is NOT an array of pairs elem
       if(children.size() == 2){
         children.get(1).toAssembly();
+        TypeID child_2 = children.get(1).getIdentifier().getType();
+        addToCurLabel(new LDR(Instr.R0, String.valueOf(child_2.getBytes())));
+        addToCurLabel(new BRANCH(true, "", "malloc"));
+        addToCurLabel(new STR(child_2.getBytes(),"", Instr.R5, Instr.R0, 0));
+        addToCurLabel(new STR(Instr.R0, Instr.R4, 4));
       }
 
-      instructions.add(new LDR(Instr.R0, "4"));
-      instructions.add(new BRANCH(true, "", "malloc"));
-      instructions.add(new STR(Instr.R5, Instr.R0, 0));
-      instructions.add(new STR(Instr.R0, Instr.R4, 4));
-
-      addToCurLabel(instructions);
+      //addToCurLabel(instructions);
 
     } else {
       for (Node expr: children) {
