9a433632fc0fc903454ea6cf8e183ee4df6c17c2
Ivy Tam <iat19@ic.ac.uk>
2021-03-03 22:54:11 +0800

    [iat19] Accounted for char and bool in PairElemAST

--- src/main/frontend/abstractsyntaxtree/pairs/PairElemAST.java
+++ src/main/frontend/abstractsyntaxtree/pairs/PairElemAST.java
@@ -11,12 +11,9 @@ import frontend.abstractsyntaxtree.expressions.ArrayElemAST;
 import frontend.abstractsyntaxtree.expressions.IdentExprAST;
 import frontend.abstractsyntaxtree.expressions.PairLiterAST;
 import frontend.errorlistener.SemanticErrorCollector;
-import frontend.symboltable.Identifier;
-import frontend.symboltable.NullID;
-import frontend.symboltable.PairID;
-import frontend.symboltable.ParamID;
-import frontend.symboltable.SymbolTable;
+import frontend.symboltable.*;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -32,8 +29,11 @@ public class PairElemAST extends Node {
   private String identName;
   private final PairElemContext ctx;
 
-  public PairElemAST(Identifier childIdentifier, SymbolTable symtab,
-      boolean first, Node child,
+  public PairElemAST(
+      Identifier childIdentifier,
+      SymbolTable symtab,
+      boolean first,
+      Node child,
       PairElemContext ctx) {
     super();
     this.childIdentifier = childIdentifier;
@@ -52,6 +52,7 @@ public class PairElemAST extends Node {
     return first;
   }
 
+
   @Override
   public void check() {
     // Checking expr has compatible type
@@ -67,10 +68,12 @@ public class PairElemAST extends Node {
       }
 
     } else if (child instanceof PairLiterAST) {
-      SemanticErrorCollector
-          .addIncompatibleType("pair",
-              "null", identName,
-              ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine());
+      SemanticErrorCollector.addIncompatibleType(
+          "pair",
+          "null",
+          identName,
+          ctx.getStart().getLine(),
+          ctx.getStart().getCharPositionInLine());
 
     } else {
       addIncompatibleTypeSemanticError();
@@ -81,53 +84,57 @@ public class PairElemAST extends Node {
       setIdentifier(childIdentifier);
     } else {
       PairID childIDAsPair =
-          childIdentifier instanceof ParamID ? ((PairID) (childIdentifier)
-              .getType())
+          childIdentifier instanceof ParamID
+              ? ((PairID) (childIdentifier).getType())
               : ((PairID) childIdentifier);
-      Identifier childIDPairElem = first ? childIDAsPair.getFstType()
-          : childIDAsPair.getSndType();
+      Identifier childIDPairElem = first ? childIDAsPair.getFstType() : childIDAsPair.getSndType();
 
-      setIdentifier(
-          Objects.requireNonNullElseGet(childIDPairElem, NullID::new));
+      setIdentifier(Objects.requireNonNullElseGet(childIDPairElem, NullID::new));
     }
   }
 
   private void addIncompatibleTypeSemanticError() {
-    SemanticErrorCollector
-        .addIncompatibleType("pair",
-            child.getIdentifier().getType().getTypeName(), identName,
-            ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine());
+    SemanticErrorCollector.addIncompatibleType(
+        "pair",
+        child.getIdentifier().getType().getTypeName(),
+        identName,
+        ctx.getStart().getLine(),
+        ctx.getStart().getCharPositionInLine());
   }
 
   @Override
   public void toAssembly() {
     List<Instr> instructions = new ArrayList<>();
 
-//    instructions.add(new STR(1, "", Instr.R5, Instr.R0, 0));
-//    instructions.add(new STR(Instr.R0, Instr.R4, 4));
-//    // use symbol table to get offset and then add the extra inner offset
+    //    instructions.add(new STR(1, "", Instr.R5, Instr.R0, 0));
+    //    instructions.add(new STR(Instr.R0, Instr.R4, 4));
+    //    // use symbol table to get offset and then add the extra inner offset
     int stackPointerOffset = symtab.getStackOffset(identName);
-//
-//    stackPointerOffset += childIdentifier.getType().getBytes();
-//
-//    instructions.add(new STR(Instr.R4, Instr.SP, stackPointerOffset));
+    //
+    //    stackPointerOffset += childIdentifier.getType().getBytes();
+    //
+    //    instructions.add(new STR(Instr.R4, Instr.SP, stackPointerOffset));
+
     instructions.add(new LDR(Instr.R4, Instr.SP, stackPointerOffset));
 
     instructions.add(new MOV("", Instr.R0, Instr.R4));
     BackEndGenerator.addToPreDefFuncs("p_check_null_pointer");
     instructions.add(new BRANCH(true, "", "p_check_null_pointer"));
 
+    PairID type = (PairID) childIdentifier.getType();
+    TypeID elem_type;
+
     if (first) {
+      elem_type = type.getFstType();
       instructions.add(new LDR(Instr.R4, Instr.R4, 0));
     } else {
+      elem_type = type.getSndType();
       instructions.add(new LDR(Instr.R4, Instr.R4, 4));
     }
 
-    instructions.add(new LDR(childIdentifier.getType().getBytes(), "", Instr.R4, Instr.R4, 0));
-//    instructions.add(new STR(Instr.R4, Instr.SP, 0));
+    instructions.add(new LDR(elem_type.getBytes(), "", Instr.R4, Instr.R4, 0));
+    //    instructions.add(new STR(Instr.R4, Instr.SP, 0));
 
     addToCurLabel(instructions);
   }
-
-
 }
