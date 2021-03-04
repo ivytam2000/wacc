35587b069670b928f606b8002b2f55b823f66837
jyw19 <jyw19@ic.ac.uk>
2021-03-05 00:57:38 +0800

    [full team] Update addressing mode to use builder pattern

--- src/main/backend/instructions/AddrMode.java
+++ src/main/backend/instructions/AddrMode.java
@@ -6,10 +6,38 @@ public class AddrMode extends Instr {
   private final int offsetFromOperand;
   private final AddrModeType type;
 
-  public AddrMode(Object operand, int offsetFromOperand, AddrModeType type) {
+  private AddrMode(Object operand, int offsetFromOperand, AddrModeType type) {
     this.operand = operand;
     this.offsetFromOperand = offsetFromOperand;
     this.type = type;
+  }
+
+  public AddrMode buildVal(Object operand) {
+    return new AddrMode(operand, 0, AddrModeType.VAL);
+  }
+
+  public AddrMode buildImm(Object operand) {
+    return new AddrMode(operand, 0, AddrModeType.IMM);
+  }
+
+  public AddrMode buildImmWithShiftL(Object operand) {
+    return new AddrMode(operand, 0, AddrModeType.IMM_SHIFT_L);
+  }
+
+  public AddrMode buildImmWithShiftR(Object operand) {
+    return new AddrMode(operand, 0, AddrModeType.IMM_SHIFT_R);
+  }
+
+  public AddrMode buildAddr(Object operand) {
+    return new AddrMode(operand, 0, AddrModeType.ADDR_OFFSET);
+  }
+
+  public AddrMode buildAddrWithOffset(Object operand, int offsetFromOperand) {
+    return new AddrMode(operand, offsetFromOperand, AddrModeType.ADDR_OFFSET);
+  }
+
+  public AddrMode buildAddrWithWriteBack(Object operand, int offsetFromOperand) {
+    return new AddrMode(operand, offsetFromOperand, AddrModeType.ADDR_OFFSET_WRITEBACK);
   }
 
   @Override
@@ -19,6 +47,10 @@ public class AddrMode extends Instr {
         return "=" + operand;
       case IMM:
         return "#" + operand;
+      case IMM_SHIFT_L:
+        return "LSL #" + operand;
+      case IMM_SHIFT_R:
+        return "LSR #" + operand;
       case ADDR_OFFSET:
         if (offsetFromOperand == 0) {
           return ("[" + operand + "]");
--- src/main/backend/instructions/AddrModeType.java
+++ src/main/backend/instructions/AddrModeType.java
@@ -1,8 +1,10 @@
 package backend.instructions;
 
-public enum AddrModeType {
+enum AddrModeType {
   VAL,
   IMM,
+  IMM_SHIFT_L,
+  IMM_SHIFT_R,
   ADDR_OFFSET,
   ADDR_OFFSET_WRITEBACK
 }
