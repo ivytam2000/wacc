117534b4001ba14821453cf5c415dec5dace42eb
Brandon <szh19@ic.ac.uk>
2021-03-16 21:40:19 +0800

    [szh19] Added support for building AST of dynamic variable type

--- src/main/frontend/symboltable/VarID.java
+++ src/main/frontend/symboltable/VarID.java
@@ -1,2 +1,13 @@
-package frontend.symboltable;public class VarID {
+package frontend.symboltable;
+
+public class VarID extends TypeID {
+
+  public VarID() {
+    super("Dynamic Variable");
+  }
+
+  @Override
+  public TypeID getType() {
+    return this;
+  }
 }
