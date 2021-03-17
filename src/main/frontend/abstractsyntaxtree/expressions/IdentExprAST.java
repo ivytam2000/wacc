c1482eaa7b48cb2e1fad9ee9cb0bebd40a10201b
Brandon <szh19@ic.ac.uk>
2021-03-17 14:36:46 +0800

    [szh19] Added type number check when loading a variable (throws a runtime error if incompatible type)

--- src/main/frontend/abstractsyntaxtree/expressions/IdentExprAST.java
+++ src/main/frontend/abstractsyntaxtree/expressions/IdentExprAST.java
@@ -1,24 +1,31 @@
 package frontend.abstractsyntaxtree.expressions;
 
 import antlr.WaccParser.IdentExprContext;
+import backend.BackEndGenerator;
+import backend.instructions.ADD;
 import backend.instructions.AddrMode;
+import backend.instructions.BRANCH;
 import backend.instructions.Condition;
 import backend.instructions.Instr;
 import backend.instructions.LDR;
+import backend.instructions.Label;
+import backend.instructions.MOV;
 import frontend.abstractsyntaxtree.Node;
+import frontend.abstractsyntaxtree.Utils;
 import frontend.errorlistener.SemanticErrorCollector;
 import frontend.symboltable.Identifier;
 import frontend.symboltable.SymbolTable;
+import frontend.symboltable.TypeID;
 import frontend.symboltable.UnknownID;
+import frontend.symboltable.VarID;
 import java.util.ArrayList;
 import java.util.List;
-
-import static backend.instructions.Instr.addToCurLabel;
 
 public class IdentExprAST extends Node {
 
   private final SymbolTable currsymtab;
   private final IdentExprContext ctx;
+  private int dynamicTypeNeeded;
 
   public IdentExprAST(SymbolTable currsymtab, IdentExprContext ctx) {
     super();
@@ -28,6 +35,10 @@ public class IdentExprAST extends Node {
 
   public String getName() {
     return ctx.getText();
+  }
+
+  public void setDynamicTypeNeeded(TypeID type) {
+    dynamicTypeNeeded = Utils.getTypeNumber(type);
   }
 
   @Override
@@ -47,10 +58,35 @@ public class IdentExprAST extends Node {
 
   @Override
   public void toAssembly() {
+    List<Instr> instrs = new ArrayList<>();
+
     // Load from (SP + offset) into target register
     int offset = currsymtab.getStackOffset(getName());
     Instr loadVar = new LDR(identifier.getType().getBytes(), Condition.NO_CON,
         Instr.getTargetReg(), AddrMode.buildAddrWithOffset(Instr.SP, offset));
-    addToCurLabel(loadVar);
+    instrs.add(loadVar);
+
+    if (identifier instanceof VarID) {
+      if (dynamicTypeNeeded < 0) {
+        System.err.println("DYNAMIC TYPE NEEDED, BUT NOT SPECIFIED");
+      } else {
+        // TODO: Can we just freely use R0 and R1? Need to save?
+
+        // Get addr into R0
+        instrs.add(new ADD(false, Instr.R0, Instr.SP, AddrMode.buildImm(offset)));
+        // Load typeNumber (byte) from "box" into R0
+        instrs.add(new LDR(-Instr.BYTE_SIZE, Condition.NO_CON, Instr.R0, AddrMode.buildAddrWithOffset(Instr.R0, Instr.WORD_SIZE)));
+        // Load actual typeNumber needed
+        instrs.add(new MOV(Condition.NO_CON, Instr.R1, AddrMode.buildImm(dynamicTypeNeeded)));
+        // Jump to dynamic type check
+        BackEndGenerator.addToPreDefFuncs(Label.P_DYNAMIC_TYPE_CHECK);
+        instrs.add(new BRANCH(true, Condition.NO_CON, Label.P_DYNAMIC_TYPE_CHECK));
+
+        // Reset dynamic type
+        dynamicTypeNeeded = -1;
+      }
+    }
+
+    Instr.addToCurLabel(instrs);
   }
 }
